084a1a11901c0e42db7206f12acc065fc071c637
Oliver Apelqvist Eriksson <oliape060414@student.jenseneducation.se>
2023-12-05 23:41:59 +0100

    ny stil

--- textpage.js
+++ textpage.js
@@ -1,33 +1,34 @@
 
 document.getElementById("titeln").innerHTML = "Den här webbplatsen ser jättekonstig ut!!!";
 
-
-function stringTillDiv(brodtextInput) {
-  let result = "";
-  result += "<p id='text'> ";
-  // Första 30 karaktärer
-  for (let i = 0; i < 100 && i < brodtextInput.length; i++) {
-    result += brodtextInput[i];
-  }
-  result += "<span class='hidden'>";
-  // Resten
-  for (let i = 100; i < brodtextInput.length; i++) {
-    result += brodtextInput[i];
-  }
-  result += "</span> </p>";
-  document.getElementById("brodtext").innerHTML = result;
-  // varför kan inte limptexten vara här??!
-}
 function stringTillDivNumeroDos(brodtextInput) {
   let result = "";
   result += brodtextInput;
   document.getElementById("limptext").innerHTML = result;
 }
 let brodtextInput = "Varför ser den här hemsidan så konstig ut? Det är som att man inte kan lägga in några posts i den ÄNNU för att PHP är det jobbigaste som finns? Det går alltså inte att få in fler textfiler för att jag ännu inte klurat ut det ÄN. Hur skulle man ens göra det, man skulle behöva någon typ av back-end troligtvis. Men det är också ganska jobbigt. Skulle man kunna göra en simpel javascript till en .txt-fil? Kanske. Hmmm";
-stringTillDiv(brodtextInput);
-stringTillDivNumeroDos("brodtextInput");
-// Limptexten kan inte vara här heller?!!
-document.getElementById("ack").innerHTML = "super aido de xiaojong";
+
+stringTillDivNumeroDos(brodtextInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function toggleReadMore() {
   var hiddenText = document.querySelector('#text .hidden');
